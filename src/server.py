3a345d7cda075f7b19ccad637f00b6ae19dd2ed8
Ilan Prais <ilandovprais@gmail.com>
2020-11-17 16:03:18 +0200

    server working on 1 layer

--- src/server.py
+++ src/server.py
@@ -5,21 +5,18 @@ class server:
     
     def __init__(self, port, clientHandler):
         self.__port = port
-        self.__fileHandler = fileHandler
+        self.__clientHandler = clientHandler
 
     def open(self):
-        clienthandler.initializeDomainsMap()
+        self.__clientHandler.initializeDomainsMap()
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-        s.bind(('', self.__myPort))
+        s.bind(('', self.__port))
 
         while True:
             data, addr = s.recvfrom(1024)   
-            result = self.handleRequest(data.decode())
+            result = self.__clientHandler.handleRequest(data.decode())
             s.sendto(result.encode(), addr)
-
-    def handleRequest(self, domain):
-        return self.__map.getProperties(domain)[0]
 
 class clienthandler:
 
@@ -29,30 +26,29 @@ class clienthandler:
         self.__parentServerIP = parentServerIP
         self.__parentServerPort = parentServerPort
 
-    def handleRequest(self, socket):
-        for d in self.__domainsMap:
-            startTime = self.__domainsMap[d][2]
-            ttl = self.__domainsMap[d][1]
-            if (currentTime - startTime > ttl):
-                del self.__domainsMap[d]
-                self.__filehandler.removeLine(d)
+    def handleRequest(self, data):
+        # for d in self.__domainsMap:
+        #     startTime = self.__domainsMap[d][2]
+        #     ttl = self.__domainsMap[d][1]
+        #     if (currentTime - startTime > ttl):
+        #         del self.__domainsMap[d]
+        #         self.__filehandler.removeLine(d)
 
-        data, addr = socket.recvfrom(1024)
-        domain = data.decode()
-            
-        if domain in self.__domainsMap:
-            return self.__domainsMap[domain][0]
-        
-        #request from the parent server to get ip
-        self.__domainsMap[ip] = ...
-        return ip
+        #check my ips
+        if data in self.__domainsMap:
+            return self.__domainsMap[data][0]
+        #check parent ips
+        else:
+            pass
+            # self.__domainsMap[ip] = ...
+            # return ip
+        return ''
 
     def initializeDomainsMap(self):
         lines = self.__fileHandler.getLines()
         for line in lines:
             properties = line.split(',')
-            self.__domainsMap[properties[0]] = (properties[1], properties[2], currentTime)
-
+            self.__domainsMap[properties[0]] = (properties[1], properties[2], 0)
 
 class filehandler:
 
@@ -70,45 +66,26 @@ class filehandler:
         return lines
 
     def addLine(self, line):
-        with open(self.__fileName, "a+") as f:
-            file.write(line)
-
-    def removeLine(self, prefix):
-        lines = self.getLines()
-        with open(self.__fileName, 'w') as f:
-            for line in lines:
-                if (line.startswith(prefix) == False):
-                    f.write(line)
-
-class mapper:
-
-    def __init__(self, fileName):
-        self.__fileName = fileName
-        self.__map = {}
-
-    def readFile(self):
-        try:
-            file = open(self.__fileName, "r")
-        except:
-            file = open(self.__fileName, "w+")
-        lines = file.readlines()
-        for line in lines:
-            properties = line.split(",")
-            self.__map[properties[0]] = (properties[1], properties[2])
+        file = open(self.__fileName, "a+")
+        file.write(line)
         file.close()
-    
-    def getProperties(self, key):
-        try:
-            return self.__map[key]
-        except:
-            return ('','')
 
-    def append(self, properties):
+    def appendEntry(self, properties):
         file = open(self.__fileName, "a+")
         self.__map[properties[0]] = (properties[1], properties[2])
         line = properties[0] + ',' + properties[1] + ',' + properties[2]
         file.write(line)
         file.close()
 
-ser = server(12345, None, None, "ips.txt")
+    def removeLine(self, prefix):
+        lines = self.getLines()
+        file = open(self.__fileName, 'w')
+        for line in lines:
+            if (line.startswith(prefix) == False):
+                file.write(line)
+        file.close()
+
+fileHandler = filehandler('ips.txt')
+clientHandler = clienthandler(fileHandler, '127.0.0.1', 12346)
+ser = server(12345, clientHandler)
 ser.open()
