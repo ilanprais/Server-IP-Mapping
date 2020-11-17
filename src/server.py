954166b38d6d64965de621656d2c97fc66c53bec
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-11-17 15:19:04 +0200

    Adding clienthandler and filehandler

--- src/server.py
+++ src/server.py
@@ -1,16 +1,14 @@
 import socket
 import sys
+
 class server:
     
-    def __init__(self, myPort, parentIP, parentPort, ipsFileName):
-        self.__myPort = myPort
-        self.__parentIP = parentIP
-        self.__parentPort = parentPort
-        self.__ipsFileName = ipsFileName
-        self.__map = mapper(ipsFileName)
+    def __init__(self, port, clientHandler):
+        self.__port = port
+        self.__fileHandler = fileHandler
 
     def open(self):
-        self.__map.readFile()
+        clienthandler.initializeDomainsMap()
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
         s.bind(('', self.__myPort))
@@ -21,7 +19,66 @@ class server:
             s.sendto(result.encode(), addr)
 
     def handleRequest(self, domain):
-        return self.__map.getProps(domain)[0]
+        return self.__map.getProperties(domain)[0]
+
+class clienthandler:
+
+    def __init__(self, fileHandler, parentServerIP, parentServerPort):
+        self.__domainsMap = {}
+        self.__fileHandler = fileHandler
+        self.__parentServerIP = parentServerIP
+        self.__parentServerPort = parentServerPort
+
+    def handleRequest(self, socket):
+        for d in self.__domainsMap:
+            startTime = self.__domainsMap[d][2]
+            ttl = self.__domainsMap[d][1]
+            if (currentTime - startTime > ttl):
+                del self.__domainsMap[d]
+                self.__filehandler.removeLine(d)
+
+        data, addr = socket.recvfrom(1024)
+        domain = data.decode()
+            
+        if domain in self.__domainsMap:
+            return self.__domainsMap[domain][0]
+        
+        #request from the parent server to get ip
+        self.__domainsMap[ip] = ...
+        return ip
+
+    def initializeDomainsMap(self):
+        lines = self.__fileHandler.getLines()
+        for line in lines:
+            properties = line.split(',')
+            self.__domainsMap[properties[0]] = (properties[1], properties[2], currentTime)
+
+
+class filehandler:
+
+    def __init__(self, fileName):
+        self.__fileName = fileName
+
+    def getLines(self):
+        try:
+            file = open(self.__fileName, "r")
+        except:
+            file = open(self.__fileName, "w+")
+
+        lines = file.readlines()
+        file.close()
+        return lines
+
+    def addLine(self, line):
+        with open(self.__fileName, "a+") as f:
+            file.write(line)
+
+    def removeLine(self, prefix):
+        lines = self.getLines()
+        with open(self.__fileName, 'w') as f:
+            for line in lines:
+                if (line.startswith(prefix) == False):
+                    f.write(line)
 
 class mapper:
 
@@ -37,22 +94,19 @@ class mapper:
         lines = file.readlines()
         for line in lines:
             properties = line.split(",")
-            self.__map[properties[0]] = [properties[1], properties[2]]
+            self.__map[properties[0]] = (properties[1], properties[2])
         file.close()
     
-    def getProps(self, key):
+    def getProperties(self, key):
         try:
             return self.__map[key]
         except:
-            return ['','']
+            return ('','')
 
-    def append(self, props):
+    def append(self, properties):
         file = open(self.__fileName, "a+")
-        self.__map[props[0]] = [props[1], props[2]]
-        line = ''
-        for prop in props:
-            line += props + ','
-        line = line[0, len(line) - 2]
+        self.__map[properties[0]] = (properties[1], properties[2])
+        line = properties[0] + ',' + properties[1] + ',' + properties[2]
         file.write(line)
         file.close()
 
