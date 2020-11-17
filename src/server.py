d8e6af1cf948078f4f2cc58e425a9595e241658d
Ilan Prais <ilandovprais@gmail.com>
2020-11-17 13:39:04 +0200

    finished server with no parent support

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 import socket
-
+import sys
 class server:
     
     def __init__(self, myPort, parentIP, parentPort, ipsFileName):
@@ -7,7 +7,7 @@ class server:
         self.__parentIP = parentIP
         self.__parentPort = parentPort
         self.__ipsFileName = ipsFileName
-        self.__map = mapper()
+        self.__map = mapper(ipsFileName)
 
     def open(self):
         self.__map.readFile()
@@ -16,32 +16,45 @@ class server:
         s.bind(('', self.__myPort))
 
         while True:
-            data, addr = s.recvfrom(1024)
-            result = self.handleRequest(data)
-            s.sendto(result, addr)
+            data, addr = s.recvfrom(1024)   
+            result = self.handleRequest(data.decode())
+            s.sendto(result.encode(), addr)
 
     def handleRequest(self, domain):
-        return self.__map.getProps(domain)[1]
+        return self.__map.getProps(domain)[0]
 
 class mapper:
 
     def __init__(self, fileName):
-        self.__file = open(self.__ipsFileName, "a+")
+        self.__fileName = fileName
         self.__map = {}
 
     def readFile(self):
-        lines = self.__file.readlines()
+        try:
+            file = open(self.__fileName, "r")
+        except:
+            file = open(self.__fileName, "w+")
+        lines = file.readlines()
         for line in lines:
             properties = line.split(",")
-            self.__map[properties[0]] = (properties[1], properties[2])
-
+            self.__map[properties[0]] = [properties[1], properties[2]]
+        file.close()
+    
     def getProps(self, key):
-        return self.__map[key]
+        try:
+            return self.__map[key]
+        except:
+            return ['','']
 
     def append(self, props):
-        self.__map[props[0]] = (props[1], props[2])
+        file = open(self.__fileName, "a+")
+        self.__map[props[0]] = [props[1], props[2]]
         line = ''
         for prop in props:
             line += props + ','
         line = line[0, len(line) - 2]
-        self.__file.write(line)
+        file.write(line)
+        file.close()
+
+ser = server(12345, None, None, "ips.txt")
+ser.open()
