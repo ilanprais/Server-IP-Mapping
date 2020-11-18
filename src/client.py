dd0718320e9730c01d1d0c688cda715ce17ba4c0
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-11-18 12:21:03 +0200

    Updating client

--- src/client.py
+++ src/client.py
@@ -1,7 +1,12 @@
 import socket
+import sys
 
+#opening the client socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'www.youtube.com', ('127.0.0.1', 12345))
-data, addr = s.recvfrom(1024)
-print(data.decode())
-s.close()
+
+#getting the user input domain, sending it to the server, and printing the answer of the server
+while True:
+    domain = input()
+    s.sendto(domain, (sys.argv[0], sys.argv[1]))
+    data, addr = s.recvfrom(1024)
+    print(data.decode())
